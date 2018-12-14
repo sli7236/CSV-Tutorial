6360fd8af43293bec5c1cae3dc73507cff5f6f51
BT_1N3_03 <sli7236@bths.edu>
2018-12-14 11:45:23 -0500

    data

--- src/sample/CSVReader.java
+++ src/sample/CSVReader.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class CSVReader {
 
     public static void main(String... args) {
-        List<Data> data = readCSVFile("2012_SAT_Results.csv");
+        List<Data> data = readCSVFile("C:\\Users\\BT_1N3_03\\IdeaProjects\\CSV-Tutorial\\src\\sample\\2012_SAT_Results.csv");
 
         // let's print all the person read from CSV file
         for (Data b : data) {
